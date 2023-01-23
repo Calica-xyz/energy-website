59621bc72e7c19109ac7f942598d8bbf2cfef2de
Nik Agarwal <thenikiscooking@gmail.com>
2023-01-23 15:40:33 -0800

    Build error fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Header from "./Header.svelte";
-  import Mission from "./Mission.svelte";
+  // import Header from "./Header.svelte";
+  // import Mission from "./Mission.svelte";
   import Navbar from "./Navbar.svelte";
-  import Service from "./Service.svelte";
-  import Signup from "./Signup.svelte";
+  // import Service from "./Service.svelte";
+  // import Signup from "./Signup.svelte";
 </script>
 
 <Navbar />
